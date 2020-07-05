8343d3d339007ead73c43f6cf1446fc2c4dbdd39
j3llyf1sh.boop <joshua.krystjan.warner@gmail.com>
2020-07-06 00:08:37 +0100

    This fixed the gem problem,made it so that they can be picked up etc, but you can still run outside the camera area, which is kind of a problem

--- DarknessGame/Assets/Scripts/Walls/WallBlocker.cs
+++ DarknessGame/Assets/Scripts/Walls/WallBlocker.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class WallBlocker : MonoBehaviour
 {
-   
+    
     void OnTriggerEnter2D(Collider2D col)
     {
-        Debug.Log("Collide");
         col.isTrigger = false;
     }
+
 }
