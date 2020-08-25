40bb0d7b0877d5e899fe1e7dec50ac06099ecbc0
j3llyf1sh.boop <joshua.krystjan.warner@gmail.com>
2020-08-25 11:06:11 +0100

    Lightsource problems seem to be fixed and working

---
+++ DarknessGame/Assets/Scripts/GameManager/MaterialChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    public GameObject[] invisibleObjects;
+    public Material invisible;
+    public void OnEnable()
+    {
+        SpriteRenderer sr;
+
+        foreach (GameObject material in invisibleObjects)
+        {
+            if(material == null)
+            {
+                return;
+            }
+
+            sr = material.GetComponent<SpriteRenderer>();
+            Debug.Log("Called");
+            sr.material = invisible;
+        }
+    }
+}
